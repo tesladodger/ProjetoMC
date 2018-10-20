0a9976392f363b4ff648cd292459e7912ca97359
tesladodger <andrerfmartins@protonmail.com>
2018-10-20 22:24:25 +0000

    Recebemos o problema do projeto. Feito: -Funcionalidade inicial do menu;

--- Main.m
+++ Main.m
@@ -1,9 +1,43 @@
-%Projeto de MC
+% Projeto de MC
+% Este programa calcula o deslocamento axial de uma barra
+% sujeita a variadas forças (escolhidas pelo utilizador),
+% e sobre apoios rigidos ou molas.
 
+% Autores:
+% André Martins, 87149
+% Francisco Graça, 90249
+% Inês Véstia, 87209
+% Instituto Superior Técnico, 2018
 
-%Autores:
-%André Martins
-%Francisco Graça
-%Inês Véstia
-%2018
+clear
+clc
+repeat = true;
+erro_in = false;
+while repeat 
+
+	printf('  EQUILIBRIO ESTATICO DE BARRAS \n ')
+	printf('\n1 - Introduza os dados da barra\n')
+	printf('2 - Barras conhecidas\n')
+	printf('0 - Terminar\n')
+	if erro_in
+		printf('Erro de input\n')
+		erro_in = false;
+	end
+	a = input('==> ', 's');
+	
+	a = str2num(a);
+
+	if a == 1  
+		printf('Opcao 1\n')
+	elseif a == 2
+		printf('Opcao 2\n')
+	elseif a == 0
+		printf('A sair\n')
+		repeat = false;
+		break;
+	else
+		erro_in = true;
+		clc
+	end
+end
 
--- README.md
+++ README.md
@@ -4,3 +4,8 @@ Repositório do projeto de MC
 
 Contribuidores:
 André Martins; Francisco Graça; Inês Véstia;
+
+
+
+Notas:
+Programado usando GNU Octave 4.4.1 em Linux, testado em Matlab em Windows.
