25d20c2d54286d519766e1604f41c41c1d969d02
tesladodger <andrerfmartins@protonmail.com>
2018-10-25 23:46:09 +0100

    Update gatherVars.m

--- gatherVars.m
+++ gatherVars.m
@@ -2,20 +2,20 @@ function gatherVars()
 
 acceptZero = false;
 
-message = 'Módulo de Young do material (MPa):\n';
+message = ('Módulo de Young do material (MPa):\n');
 ymodul = getVar(acceptZero, message);
 
-message = 'Área da secção reta (m^2):\n';
+message = ('Área da secção reta (m^2):\n');
 area = getVar(acceptZero, message);
 
-message = 'Comprimento da barra (m):\n';
+message = ('Comprimento da barra (m):\n');
 comp = getVar(acceptZero, message);
 
 clc
 isMola1 = input('O primeiro apoio é uma mola? [Y/n]: ', 's');
 if isMola1 == 'y' || isMola1 == 'Y' || size(isMola1) == 0
 	isMola1 = true;
-	message = 'Constante da mola:\n';
+	message = ('Constante da mola:\n');
 	k1 = getVar(acceptZero, message);
 end
 
@@ -23,7 +23,7 @@ clc
 isMola2 = input('O segundo apoio é uma mola? [Y/n]: ', 's');
 if isMola2 == 'y' || isMola2 == 'Y' || size(isMola2) == 0
 	isMola2 = true;
-	message = 'Constante da mola:\n';
+	message = ('Constante da mola:\n');
 	k2 = getVar(acceptZero, message);
 end
 
