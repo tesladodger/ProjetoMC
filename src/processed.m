bcc1dba6a3fcbd2af42d6d227e138dcc5d20b312
tesladodger <andrerfmartins@protonmail.com>
2018-11-09 09:07:21 +0000

    Update processed.m

--- src/processed.m
+++ src/processed.m
@@ -5,3 +5,6 @@ for i = 1:N
     fprintf([reverseStr, msg]);
     reverseStr = repmat(sprintf('\b'), 1, length(msg));
 end
+
+% Não está em uso! Pode dar jeito quando estivermos a fazer 
+% cálculos com muitos pontos, fornecendo feedback visual.
