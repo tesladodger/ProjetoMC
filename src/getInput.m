0d18158acda65880a815bd3e9aca7a348fc880a9
tesladodger <andrerfmartins@protonmail.com>
2018-11-10 15:01:22 +0000

    comments

--- src/getInput.m
+++ src/getInput.m
@@ -1,8 +1,15 @@
 function someVar = getInput(message, state, canBeNegative)
 
 % Todos os inputs numéricos são tratados por 
-% esta função. Aceita valores positivos ou
+% esta função. Aceita valores positivos e
 % negativos, conforme necessário.
+% Argumentos de entrada: 
+% 	string  -> message       " mensagem a mostrar ao utilizador
+%	int     -> state         " ver drawState
+%	boolean -> canBeNegative " aceitar valores negativos ou não
+% Argumentos de saída:
+%	double  -> someVar       " variável pedida
+
 
 clc
 drawState(state);
@@ -11,26 +18,26 @@ badInput = true;
 while badInput
 	try
 		someVar = input('==> ');
-	catch   error
+	catch error
 		clc
 		drawState(state);
 		printf(message);
 		printError(0);
 		continue
 	end
-	if      size(someVar) == 0
+	if  size(someVar) == 0
 		clc
 		drawState(state);
 		printf(message);
 		printError(0);
 		continue
-	elseif  someVar < 0 && !canBeNegative
+	elseif someVar < 0 && !canBeNegative
 		clc
 		drawState(state);
 		printf(message);
 		printError(1);
 		continue
-	elseif  someVar == 0 && !canBeNegative
+	elseif someVar == 0 && !canBeNegative
 		clc
 		drawState(state);
 		printf(message);
