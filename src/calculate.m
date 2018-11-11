27069d4456adba55c58e8806079404b83da8bf8e
tesladodger <andrerfmartins@protonmail.com>
2018-11-11 19:40:57 +0000

    analiticas para sen

--- src/calculate.m
+++ src/calculate.m
@@ -1,6 +1,8 @@
 function calculate(data)
 
-format short % Demora demasiado tempo em long...
+% Esta função efectua os cálculos do deslocamento, por diferenças finitas
+% e, se a função não for genérica, calcula o valor real e o erro;
+% Posteriormente, são desenhados os gráficos desejados.
 
 
 function u = calcularDeslocamentoEntreParedes;
@@ -12,11 +14,11 @@ function u = calcularDeslocamentoEntreParedes;
 		for j = 2 : 1 : n-1
 			x = h*(j-1); % É por isto que indices começam do zero!!!
 			u(2,i) += ( invMatrix(i,j) * ( -1*(h.^2) * f(x,L) / ( E * A ) ) );
-
-			msg = sprintf('Processado %d/%d', (i*(n-1))+j, n*n);
-			printf([reverseStr, msg])
-			reverseStr = repmat(sprintf('\b'), 1, length(msg));
 		end
+
+		msg = sprintf('Processado: %d/%d', i, n);
+		printf([reverseStr, msg])
+		reverseStr = repmat(sprintf('\b'), 1, length(msg));
 
 		ponto += h;
 	end
@@ -32,16 +34,19 @@ function u = calcularDeslocamentoComForca;
 			x = h*(j-1); % É por isto que indices começam do zero!!!
 			u(2,i) += ( invMatrix(i,j) * ( -1*(h.^2) * f(x,L) / ( E * A ) ) );
 
-			msg = sprintf('Processado %d/%d', (i*(n-1))+j, n*n);
-			printf([reverseStr, msg])
-			reverseStr = repmat(sprintf('\b'), 1, length(msg));
 		end
 		u(2,i) += ( invMatrix(i,n) * ( h * F * L / ( A * E ) ) );
+
+		msg = sprintf('Processado: %d/%d', i, n);
+		printf([reverseStr, msg])
+		reverseStr = repmat(sprintf('\b'), 1, length(msg));
+	
 		ponto += h;
 	end
 end
 
 
+% ___________________________________________________________
 n = double(data.n);
 h = data.comp / n;
 f = data.cargaAxial;
@@ -49,7 +54,8 @@ E = data.ymodul;
 L = data.comp;
 A = data.area;
 
-printf('A criar a matrix de coeficientes...\n')
+
+printf('A criar a matriz dos coeficientes...\n')
 reverseStr  = '';
 matrix      = zeros(n,n);
 matrix(1,1) = 1;
@@ -58,7 +64,7 @@ for i = 2 : 1 : n-1
 	matrix(i,i+1) =  1;
 	matrix(i,i-1) =  1;
 	
-	msg = sprintf('Processado %d/%d', i+1, n);
+	msg = sprintf('Processado: %d/%d', i+1, n);
 	printf([reverseStr, msg])
 	reverseStr = repmat(sprintf('\b'), 1, length(msg));
 end
@@ -82,15 +88,32 @@ elseif data.state >= 2
 	u = calcularDeslocamentoComForca;
 end
 
+if data.funcstr == 'sen(πx/L)'
+	if data.state == 0
+		data.deslAnalit = @(x,L,E,A) ( ( L.^2 * sin(pi*x/L) ) / ( pi.^2 * E * A ) );
+	elseif data.state <= 2
+		data.deslAnalit = @(x,L,E,A) (  ( ( L.^2 * sin(pi*x/L) ) / ( pi.^2 * E * A ) ) + ( x * F / ( A * E ) )  );
+	end
+end
+
 
 printf('\nA criar o gráfico...')
 for k = 1 : 1 : n
-	X(k) = u(1,k);
-	Y(k) = u(2,k);
+	X(k)  = u(1,k);
+	DF(k) = u(2,k);                        % Diferenças finitas
+	YA(k) = data.deslAnalit(u(1,k),L,E,A); % Analiticamente
 end
-scatter(X,Y)
+figure
+scatter(X,DF,'*')
+hold on
+scatter(X,YA)
+title('Gráfico do deslocamento')
+xlabel('x (m)')
+ylabel('u(x) (m)')
+legend('Diferenças finitas','Analiticamente')
+
+
 pause
-format long
 
 
-end
+end % if you end a nested function you need to end the parent
