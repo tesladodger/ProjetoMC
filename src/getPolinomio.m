9154258f70629ec748ee17483ccbce7e2fed1197
tesladodger <andrerfmartins@protonmail.com>
2018-11-14 09:22:00 +0000

    mostrar polinomio e rever/corrigir alguns bugs resolvidos

--- src/getPolinomio.m
+++ src/getPolinomio.m
@@ -2,32 +2,55 @@ function [pol, funcstr, coef] = getPolinomio()
 
 % Função para criar um polinómio
 
-for i = 6 : -1 : 0
-	message   = sprintf('Coefiente do monómio de ordem %d :\n', i);
-	coef(i+1) = getInput(message, 401, 1); % É por causa disto que
-end                                        % indices começam do zero!!!
-% É por causa disto que indices começam do zero!!!
-pol = @(x,L) ( coef(7)*x^6 + coef(6)*x^5 + coef(5)*x^4 + coef(4)*x^3 + coef(3)*x^2 + coef(2)*x + coef(1) );
-% É por causa disto que indices começam do zero!!!
+function getFunky()
+	suffix{1} = '';
+	suffix{2} = 'x';
+	suffix{3} = 'x²';
+	suffix{4} = 'x³';
+	suffix{5} = 'x⁴';
+	suffix{6} = 'x⁵';
+	suffix{7} = 'x⁶';
+	funcstr   = '';
+	coef(8)   = 0;
+	counter   = 0;
+	for i = 7 : -1 : 1
 
-
-suffix{1} = ' ';
-suffix{2} = 'x ';
-suffix{3} = 'x² ';
-suffix{4} = 'x³ ';
-suffix{5} = 'x⁴ ';
-suffix{6} = 'x⁵ ';
-suffix{7} = 'x⁶ ';
-
-funcstr = '';
-
-for i = 7 : -1 : 1
-	if !(coef(i) == 0)
-		if (coef(i) > 0) && !(i == 7)
-			coefstr = strcat( '+', num2str(coef(i)) , suffix{i} );
-		else
-			coefstr = strcat( num2str(coef(i)) , suffix{i} );
+		if !(coef(i+1) == 0)
+			if (coef(i+1) > 0) && (counter > 1)
+				coefstr = strcat( '+', num2str(coef(i+1)) , suffix{i+1} );
+			else
+				coefstr = strcat( num2str(coef(i+1)) , suffix{i+1} );
+			end
+			funcstr = strcat( funcstr , coefstr);
 		end
-		funcstr = strcat( funcstr , coefstr);
+
+
+		message = sprintf('%s \nCoefiente do monómio de ordem %d:\n', funcstr, i-1);
+		coef(i) = getInput(message, 401, 1);
+
+		if !(coef(i) == 0)
+			counter += 1;
+		end
+
+	end
+	
+	% É por causa disto que indices começam do zero!!!
+	pol = @(x,L) ( coef(7)*x^6 + coef(6)*x^5 + coef(5)*x^4 + coef(4)*x^3 + coef(3)*x^2 + coef(2)*x + coef(1) );
+	% É por causa disto que indices começam do zero!!!
+end
+
+
+while true
+	getFunky();
+
+	clc
+	printf('Polinómio: %s\n',funcstr)
+	a = input('Pretende continuar? [Y/n] ', 's');
+
+	if a == 'y' || a == 'Y' || size(a) == 0
+		return;
 	end
 end
+
+
+end  % If nested functions are present, all functions must be ended
