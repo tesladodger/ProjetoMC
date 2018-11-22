b1b8f88c183e9133870ea3dc3765d26bdd13d0be
tesladodger <andrerfmartins@protonmail.com>
2018-11-22 11:50:26 +0000

    erro: k maiúsculo

--- src/createMatrix.m
+++ src/createMatrix.m
@@ -73,7 +73,7 @@ elseif data.pontos == 5
 		matrix(n,n-3) = -4/3;
 		matrix(n,n-2) =    3;
 		matrix(n,n-1) =   -4;
-		matrix(n,n)   =  (25/12)-((h*K)/(E*A));
+		matrix(n,n)   =  (25/12)-((h*k)/(E*A));
 	elseif data.state == 0
 		matrix(n,n) = 1/12;
 	end
